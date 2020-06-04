a9957b01305df89673a0909d8d598c443774f6b6
Abby P Joby <abbypjoby@gmail.com>
2020-06-04 22:20:33 +0530

    added terms, restructure ui

--- app/src/main/java/com/f2h/f2h_buyer/screens/settings/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/f2h/f2h_buyer/screens/settings/edit_profile/EditProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -54,6 +55,9 @@ class EditProfileFragment : Fragment() {
         viewModel.toastText.observe(viewLifecycleOwner, Observer { message ->
             Toast.makeText(this.context, message, Toast.LENGTH_SHORT).show()
         })
+
+        //Set app bar title to group name here
+        (context as AppCompatActivity).supportActionBar!!.title = "Edit Profile"
 
         return binding.root
     }
