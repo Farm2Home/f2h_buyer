dc9bb3613ecfec162094c27040558a9b7faa0093
Rahul Roy <rahulroy1996@gmail.com>
2020-08-03 22:24:37 +0530

    otp

--- app/src/main/java/com/f2h/f2h_buyer/screens/signup/SignUpFragment.kt
+++ app/src/main/java/com/f2h/f2h_buyer/screens/signup/SignUpFragment.kt
@@ -2,6 +2,7 @@ package com.f2h.f2h_buyer.screens.signup
 
 import android.app.Application
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,9 +16,14 @@ import com.f2h.f2h_buyer.R
 import com.f2h.f2h_buyer.database.F2HDatabase
 import com.f2h.f2h_buyer.database.SessionDatabaseDao
 import com.f2h.f2h_buyer.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class SignUpFragment: Fragment() {
-
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentSignupBinding
     private val application: Application by lazy { requireNotNull(this.activity).application }
     private val dataSource: SessionDatabaseDao by lazy { F2HDatabase.getInstance(application).sessionDatabaseDao }
@@ -29,6 +35,7 @@ class SignUpFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        auth = FirebaseAuth.getInstance()
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false)
         binding.viewModel = viewModel
         binding.setLifecycleOwner(this)
@@ -43,7 +50,92 @@ class SignUpFragment: Fragment() {
             Toast.makeText(this.context, message, Toast.LENGTH_SHORT).show()
         })
 
+        viewModel.isSendOtpClicked.observe(viewLifecycleOwner, Observer { sendOtpClicked ->
+            if (sendOtpClicked){
+                startPhoneNumberVerification(viewModel.mobile.value?:"")
+
+            }
+        })
+
+        viewModel.verifyOtpClicked.observe(viewLifecycleOwner, Observer { verifyOtpClicked ->
+            if (verifyOtpClicked){
+                val credential = PhoneAuthProvider.getCredential(viewModel.mVerificationId!!, viewModel.otp.value!!)
+                signInWithPhoneAuthCredential(credential)
+            }
+        })
+
+        viewModel.isVerifyingOtp.observe(viewLifecycleOwner, Observer { isVerifyingOtp ->
+            if (isVerifyingOtp){
+                object : CountDownTimer(60000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        binding.resend.setText((millisUntilFinished / 1000).toInt().toString())
+                    }
+
+                    override fun onFinish() {
+                        binding.resend.setText("Resend OTP")
+                        binding.resend.isEnabled = true
+                    }
+                }.start()
+            }
+        })
+
+        viewModel.resendOtpClicked.observe(viewLifecycleOwner, Observer { resendOtpClicked ->
+            binding.resend.isEnabled = false
+            if (resendOtpClicked) {
+                object : CountDownTimer(60000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        binding.resend.setText((millisUntilFinished / 1000).toInt().toString())
+                    }
+
+                    override fun onFinish() {
+                        binding.resend.setText("Resend OTP")
+                        binding.resend.isEnabled = true
+                    }
+                }.start()
+            }
+        })
+
+
+
         return binding.root
+    }
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        // [START start_phone_auth]
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            "+91"+phoneNumber, // Phone number to verify
+            60,             // Timeout duration
+            TimeUnit.SECONDS,   // Unit of timeout
+            requireActivity(),           // Activity (for callback binding)
+            viewModel.mCallbacks)        // OnVerificationStateChangedCallbacks
+        // [END start_phone_ath]
+    }
+
+    private fun resendVerificationCode(phoneNumber: String,
+                                       token: PhoneAuthProvider.ForceResendingToken?) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            "+91"+phoneNumber, // Phone number to verify
+            60, // Timeout duration
+            TimeUnit.SECONDS, // Unit of timeout
+            requireActivity(), // Activity (for callback binding)
+            viewModel.mCallbacks, // OnVerificationStateChangedCallbacks
+            token)             // ForceResendingToken from callbacks
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    viewModel.updateUi(4)
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                        viewModel.updateUi(3)
+                    }
+                }
+            }
     }
 
 
@@ -52,5 +144,6 @@ class SignUpFragment: Fragment() {
         val action = SignUpFragmentDirections.actionSignUpFragmentToLoginFragment()
         view?.let { Navigation.findNavController(it).navigate(action) }
     }
+
 
 }
