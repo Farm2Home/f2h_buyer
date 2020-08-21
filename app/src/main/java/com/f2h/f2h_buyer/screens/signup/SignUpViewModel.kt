ff55b0f62e6b5f4ee908ee6346b1cc5f7dfffb22
Rahul Roy <rahulroy1996@gmail.com>
2020-08-21 23:07:25 +0530

    sign up bug

--- app/src/main/java/com/f2h/f2h_buyer/screens/signup/SignUpViewModel.kt
+++ app/src/main/java/com/f2h/f2h_buyer/screens/signup/SignUpViewModel.kt
@@ -296,8 +296,7 @@ class SignUpViewModel(val database: SessionDatabaseDao, application: Application
         userObject.userName = userName.value
         var address = arrayListOf(locality.value?:"", city.value?:"", state.value?:"",
             pincode.value?:"")
-        address.removeIf(String::isEmpty)
-        userObject.address  = address.joinToString()
+        userObject.address  = address.filter{x -> !x.isNullOrEmpty()}.joinToString()
         userObject.mobile = mobile.value
         userObject.email = email.value
         userObject.password = Base64.encodeToString(password.value?.toByteArray(), DEFAULT)
