fda7af9d8ec0f5b787c4aa14c411ed6d56e3d728
abbypjoby <abbypjoby@gmail.com>
2020-08-30 00:16:56 +0530

    notification bug fix

--- app/src/main/java/com/f2h/f2h_buyer/utils/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/f2h/f2h_buyer/utils/MyFirebaseMessagingService.kt
@@ -1,10 +1,21 @@
 package com.f2h.f2h_buyer.utils
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.f2h.f2h_buyer.R
 import com.f2h.f2h_buyer.database.*
 import com.f2h.f2h_buyer.network.UserApi
 import com.f2h.f2h_buyer.network.models.UserCreateRequest
+import com.f2h.f2h_buyer.screens.MainActivity
+import com.f2h.f2h_buyer.screens.UserPagesActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.*
@@ -19,11 +30,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
 
-        Log.i(TAG, "From: " + remoteMessage.from)
-        Log.i(TAG, "Notification Message Body: " + remoteMessage.notification?.body!!)
-
-
-
         var body = ""
         var title = ""
         if (remoteMessage.data.isNotEmpty()){
@@ -33,20 +39,18 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             }
             if(extras.containsKey("body") && !extras.getString("body").isNullOrBlank()) {
                 body = extras.getString("body")!!
-//                saveNotification(extras.getString("message")!!)
             }
             if(extras.containsKey("title") && !extras.getString("title").isNullOrBlank()) {
                 title = extras.getString("title")!!
-//                saveNotification(extras.getString("message")!!)
             }
             saveNotification(title, body)
+            sendNotification(title, body)
         }
 
     }
 
 
     private fun saveNotification(messageTitle:String, messageBody: String) {
-
         coroutineScope.launch {
             var notification = NotificationEntity(title = messageTitle, body = messageBody)
             saveNotificationInDB(notification)
@@ -110,5 +114,42 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
+
+    private fun sendNotification(title: String, body: String){
+        createNotificationChannel()
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        var builder = NotificationCompat.Builder(this, "100")
+            .setSmallIcon(R.drawable.main_logo)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(1, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("100", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
 
 }
